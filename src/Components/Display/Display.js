bfe7f0f74374e17cb6e76316fd6093eda38cfe0b
Prince Thind <princethind799@gmail.com>
2023-05-08 18:55:31 +0530

    fix stuff

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import "./Display.css"
-import {
-  NavLink, useHistory, useLocation
-} from "react-router-dom";
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import "./Display.css";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export const Display = (props) => {
-  const list = useSelector((state) => state.hosreducers.list)
- 
- 
+  const data = props.data;
 
-  console.log("dispklay page is running",list);
   return (
-    <div>
-      
-      {list.map((elem) => {
-        return (
-          <div key={elem.id}>
-            <h1>redux data</h1>
-            <h3>{elem.data}</h3>
-          </div>
-        )
-      })}
+    <div style={{ color: "white" }}>
+      data is:
+      {JSON.stringify(data, null, 2)}
     </div>
-  )
-}
+  );
+  // return (
+  //   <div>
+  //     {list.map((elem) => {
+  //       return (
+  //         <div key={elem.id}>
+  //           <h1>redux data</h1>
+  //           <h3>{elem.data}</h3>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
+};
